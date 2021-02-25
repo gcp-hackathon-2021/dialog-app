65ba7755b8f5e3e66becc8430aed2211bed15328
mishrasambit <sambitmishra987@gmail.com>
2021-02-25 20:19:00 +0530

    handling some error scenario

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,6 +5,7 @@ import { ApiService } from '../api.service';
 //import { SpeechtotextService } from '../speechtotext.service';
 import { TexttospeechService } from '../texttospeech.service';
 declare var webkitSpeechRecognition: any;
+var SpeechRecognition: any = SpeechRecognition || webkitSpeechRecognition;
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -14,7 +15,7 @@ export class HomePage {
   todos: string
   micon = false;
   micoff= true;
-  recognition =  new webkitSpeechRecognition();
+  recognition =  new SpeechRecognition();
   isStoppedSpeechRecog = false;
   text: string;
   tempWords: string;
@@ -31,14 +32,25 @@ export class HomePage {
   init() {
     this.recognition.interimResults = true;
     this.recognition.lang = 'en-IN';
-    this.recognition.addEventListener('result', (e) => {
-      const transcript = Array.from(e.results)
-        .map((result) => result[0])
-        .map((result) => result.transcript)
-        .join('');
-      this.tempWords = transcript;
-      console.log(transcript);
-    });
+    this.recognition.onresult = function(event) {
+      const transcript = Array.from(event.results)
+           .map((result) => result[0])
+           .map((result) => result.transcript)
+           .join('');
+         this.tempWords = transcript;
+         console.log(transcript);
+    }
+    this.recognition.onaudiostart = function() {
+      console.log('Audio capturing started');
+    }
+    // this.recognition.addEventListener('result', (e) => {
+    //   const transcript = Array.from(e.results)
+    //     .map((result) => result[0])
+    //     .map((result) => result.transcript)
+    //     .join('');
+    //   this.tempWords = transcript;
+    //   console.log(transcript);
+    // });
   }
 
   startListening(){
@@ -50,16 +62,25 @@ export class HomePage {
     console.log("Speech recognition started")
     this.recognition.addEventListener('end', (condition) => {
       if (this.isStoppedSpeechRecog) {
-        this.recognition.stop();
+        this.stopListening();
         console.log("End speech recognition")
       } else {
         this.text=this.tempWords;
-        this.apiService.getNews(this.tempWords).subscribe((data)=>{
-          this.todos = data['title'];
-          this.texttospeechService.speak(this.todos);
-          this.tempWords = '';
-          setTimeout(()=>{this.recognition.start();},5000);          
-        });        
+        console.log(this.tempWords);
+        if(!this.tempWords || ""===this.tempWords || undefined===this.tempWords){
+          this.tempWords = '';     
+          setTimeout(()=>{this.stopListening();},100);
+        }else{
+          this.apiService.getNews(this.tempWords).subscribe((data)=>{
+            this.todos = data['title'];
+            this.tempWords = '';
+            this.texttospeechService.speak(this.todos);
+          },(error) => {
+            console.error('error caught in component');
+            this.tempWords = '';     
+            setTimeout(()=>{this.recognition.start();},100);
+          });
+        }
       }
     });    
   }
